8ad9538c832a00c1633af5a499458ef0ea70a4de
Bass97345 <kirirlldodatko@bk.ru>
2025-05-12 16:11:14 +0300

    Добавьте файлы проекта.

---
+++ Car/Avto.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    class Avto
+    {
+        public int Number;
+        
+        private int Bak;
+        private float Top;
+        private float Ras;
+        private int Probeg = 0;
+        private float ras1km;
+        private float speed;
+        public Avto(int number, int bak, float top, float ras)
+        {
+            Number = number;
+            Bak = bak;
+            Top = top;
+            Ras = ras;
+            ras1km = ras / 100;
+        }
+
+
+        public void Gaz(float amount)
+        {
+            speed += amount;
+            Console.WriteLine($"Машина разогналась до {speed}км/ч");
+        }
+
+
+        public void Brake(float amount)
+        {
+            speed -= amount;
+            if (speed < 0) speed = 0;
+            Console.WriteLine($"Машина снизила скорость до {speed}км/ч");
+        }
+
+        public virtual void Out()
+        {
+
+
+            Console.WriteLine($"Номер машины: {Number}\nОбъём бака: {Bak}л\nТопливо: {Top}\nРасход: {Ras} л/100км\nПробег: {Probeg}км");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+        }
+
+
+        public virtual void Zapravka(float top)
+        {
+            if (top > Bak)
+            {
+                Console.WriteLine("Вы не можете залить топлива больше чем объём бака, теперь ваш бак полон");
+                Top = Bak;
+            }
+
+
+            Top += top;
+            Console.WriteLine($"Вы заправились! Топлива в баке: {Bak}л");
+
+        }
+
+
+        public virtual void Move()
+        {
+            Console.WriteLine("Введите начальную координату X");
+            int startx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите начальную координату Y");
+            int starty = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конечную координату X");
+            int endx = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите конечную координату Y");
+            int endy = int.Parse(Console.ReadLine());
+
+            int dx = endx - startx;
+            int dy = endy - starty;
+            double way = Math.Sqrt(dx * dx + dy * dy);
+
+
+
+
+            int resultKm = 0;
+
+            for (int i = 0; i < way; i++)
+            {
+                if (Top <= 0)
+                {
+                    Console.WriteLine($"Вы проехали: {resultKm}км и топливо кончилось, дозаправится?\n1.Да\n2.Нет(завершить поездку)");
+                    int zap = int.Parse(Console.ReadLine());
+
+                    if (zap == 1)
+                    {
+                        Console.WriteLine("Введите количество топлива");
+                        int top = int.Parse(Console.ReadLine())!;
+                        Zapravka(top);
+                    }else if (zap == 2)
+                    {
+                        break;
+                    }
+
+                }
+
+                Top -= ras1km;
+                resultKm += 1;
+            }
+
+            Console.WriteLine($"Машина проехала: {resultKm}км");
+            Ostatok();
+            WholeProber(resultKm);
+
+        }
+
+
+
+
+        private void WholeProber(int amount) 
+        {
+            Probeg += amount;
+            Console.WriteLine($"Общий пробег автомобиля: {Probeg}км");
+        }
+
+
+        private void Ostatok()
+        {
+            if (Top < 0) Top = 0;
+            Console.WriteLine($"Остаток топлива: {Top}л");
+        }
+
+
+
+        public void Crash(Avto car2)
+        {
+            Console.WriteLine($"Машина с номером: {Number} врезалась в машину с номером: {car2.Number}");
+        }
+
+    } 
+}
+
---
+++ Car/Program.cs
@@ -0,0 +1,242 @@
+﻿namespace Car
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+
+            List<Avto> Cars = new List<Avto>();
+
+
+
+            int num;
+            int ras;
+           
+
+
+
+            Console.WriteLine("Ввидите количество машин");
+            string countCar = Console.ReadLine();
+
+            while (IsValidNumber(countCar) == false || int.Parse(countCar) < 0 || int.Parse(countCar) > 100)
+            {
+                if (int.Parse(countCar) < 0 || int.Parse(countCar) > 100)
+                {
+                    Console.WriteLine("Число не должно быть меньше 0 и больше 100");
+                }
+                Console.WriteLine("Ввидите число");
+                countCar = Console.ReadLine();
+            }
+
+            for (int i = 0; i < int.Parse(countCar); i++)
+            {
+                num = i + 1;
+                ras = rnd.Next(10, 31);
+
+                Console.WriteLine("Ввидите объём бака");
+                string bak = Console.ReadLine();
+
+                while (IsValidNumber(bak) == false || int.Parse(bak) < 0)
+                {
+                    if (int.Parse(bak) < 0)
+                    {
+                        Console.WriteLine("Число не должно быть меньше 0");
+                    }
+                    Console.WriteLine("Ввидите число");
+                    bak = Console.ReadLine();
+                }
+
+
+
+
+                Console.WriteLine("Ввидите количество топлива");
+                string top = Console.ReadLine();
+
+                while (IsValidNumberFloat(top) == false || int.Parse(top) < 0 || int.Parse(top) > int.Parse(bak) )
+                {
+                    if (int.Parse(top) < 0 || int.Parse(top) > int.Parse(bak))
+                    {
+                        Console.WriteLine("Число не должно быть меньше 0 или больше объёма бака");
+                    }
+                    Console.WriteLine("Ввидите число");
+                    top = Console.ReadLine();
+                }
+
+                Avto myCar = new Avto(num, int.Parse(bak), float.Parse(top), ras);
+                Cars.Add(myCar);
+
+            }
+
+
+            while (true)
+            {
+
+                foreach (Avto avto in Cars)
+                {
+                    avto.Out();
+                }
+
+
+                Console.WriteLine("Ввидите число машины на которой будете ехать либо введите -1 что бы завершить работу");
+                string chos = Console.ReadLine();
+
+                while (IsValidNumber(chos) == false)
+                {
+                    Console.WriteLine("Ввидите число");
+                    countCar = Console.ReadLine();
+                }
+
+                if (int.Parse(chos) == -1)
+                {
+                    Environment.Exit(0);
+                }
+
+                var curAvto = Cars.FirstOrDefault(X => X.Number == int.Parse(chos));
+
+                while (IsValidNumber(chos) == false || curAvto == null)
+                {
+                    if (curAvto == null)
+                    {
+                        Console.WriteLine("Машины не сущетвует");
+                    }
+                    Console.WriteLine("Ввидите число");
+                    chos = Console.ReadLine();
+                    curAvto = Cars.FirstOrDefault(X => X.Number == int.Parse(chos));
+                }
+                bool end = true;
+                while (end == true)
+                {
+                    Console.WriteLine("Выберите  действие:\n1. Вывести ифнормацию о машине\n2. Ехать\n3. Заправится\n4. Разогнаться\n5. Тормозить\n6. Авария\n7. Завершить поездку");
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("Введите номер действия: ");
+                    Console.ResetColor();
+                    string chose = Console.ReadLine()!;
+
+                    while (IsValidNumber(chose) == false || (int.Parse(chose) < 1) || (int.Parse(chose) > 7))
+                    {
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Пожалуйста, введите что то из выше указаного");
+                        Console.ResetColor();
+
+                        chose = Console.ReadLine();
+                    }
+
+
+                    switch (int.Parse(chose))
+                    {
+                        case 1:
+                            curAvto.Out();
+                            break;
+                        case 2:
+                            int km = rnd.Next(0, 2000);
+                            curAvto.Move();
+                            break;
+                        case 3:
+                            Console.WriteLine("Введите количество топлива");
+
+                            string top = Console.ReadLine()!;
+
+                            while (IsValidNumber(top) == false || int.Parse(top) == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Пожалуйста, введите число или не вводите число меньше 0");
+                                Console.ResetColor();
+                                top = Console.ReadLine();
+                            }
+
+                            curAvto.Zapravka(int.Parse(top));
+                            break;
+                        case 4:
+                            Console.WriteLine("Введите на сколько км/ч разогнаться");
+
+                            string speed = Console.ReadLine()!;
+
+                            while (IsValidNumber(speed) == false || int.Parse(speed) == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Пожалуйста, введите число или не вводите число меньше 0");
+                                Console.ResetColor();
+                                speed = Console.ReadLine();
+                            }
+
+                            curAvto.Gaz(int.Parse(speed));
+                            break;
+                        case 5:
+                            Console.WriteLine("Введите на сколько км/ч тормозить");
+
+                            string stop = Console.ReadLine()!;
+
+                            while (IsValidNumber(stop) == false || int.Parse(stop) == 0)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Пожалуйста, введите число или не вводите число меньше 0");
+                                Console.ResetColor();
+                                stop = Console.ReadLine();
+                            }
+
+                            curAvto.Brake(int.Parse(stop));
+                            break;
+                        case 6:
+                            if(Cars.Count() == 1)
+                            {
+                                Console.WriteLine("Нет машин с которыми может произоти ДТП");
+                            }
+
+                            int dtp = rnd.Next(1, Cars.Count()+1);
+
+                            while (dtp == curAvto.Number)
+                            {
+                                dtp = rnd.Next(1, Cars.Count() + 1);
+                            }
+
+                            var dtpCar = Cars.FirstOrDefault(X => X.Number == dtp);
+
+                            curAvto.Crash(dtpCar);
+
+
+                            break;
+                        case 7:
+                            end = false;
+                            break;
+                    }
+
+                }
+
+
+
+
+
+            }
+        }
+        public static bool IsValidNumber(string number)
+        {
+            int juk;
+            if (int.TryParse(number, out juk))
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+
+        public static bool IsValidNumberFloat(string number)
+        {
+            float juk;
+            if (float.TryParse(number, out juk))
+            {
+                return true;
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+
+    }
+}
